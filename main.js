7c06ea72203faf3e38944ecb4783dc9a550f5584
Jeffersonmk <jefferson20042011@protonmail.com>
2024-04-11 18:10:57 -0300

    mod: index.html, main.css and main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form-phone-book');
+form.addEventListener('submit', function(e) {
+    e.preventdDefault();
+
+    const inputNomeAgenda = document.getElementById('nome-agenda');
+    const inputNumeroAgenda = document.getElementById('numero-agenda');
+})
